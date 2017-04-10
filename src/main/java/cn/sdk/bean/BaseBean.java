7850cc3210caba47cc07d9ecb63db585cf2e81bc
qinyongbo <qinyongbo@chudaokeji.com>
2017-04-10 10:38:30 +0800

    add  基类

---
+++ src/main/java/cn/sdk/bean/BaseBean.java
@@ -0,0 +1,30 @@
+package cn.sdk.bean;
+/**
+ * bean基类
+ * @author Mbenben
+ */
+public class BaseBean {
+	
+	private String code;
+	private String msg;
+	private Object data;
+	
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	public Object getData() {
+		return data;
+	}
+	public void setData(Object data) {
+		this.data = data;
+	}
+}
