3664976853e62e8a4a009b08c666585e5f17cb66
gaoxigang <gaoxigang@chudaokeji.com>
2017-05-26 15:26:53 +0800

    增加msgCode

--- src/main/java/cn/sdk/util/SzgaMsgCode.java
+++ src/main/java/cn/sdk/util/SzgaMsgCode.java
@@ -1,10 +1,38 @@
 package cn.sdk.util;
 
 public class SzgaMsgCode {
+	/**
+	 * 成功
+	 */
 	public static final String SUCCESS = "0000";
-	public static final String SYSTEM_ERROR = "500"; //系统异常
+	
+	/**
+	 * 系统异常
+	 */
+	public static final String SYSTEM_ERROR = "500";
+	
+	/**
+	 * 系统异常
+	 */
 	public static final String SYSTEM_ERROR_MSG = "系统异常";
-	public static final String PRARAM_ERROR = "0001"; //参数错误
-	public static final String SERVICE_ERROR = "0002"; //业务异常
+	
+	/**
+	 * 参数错误
+	 */
+	public static final String PRARAM_ERROR = "0001"; 
+	
+	/**
+	 * 业务异常
+	 */
+	public static final String SERVICE_ERROR = "0002"; 
+	
+	/**
+	 * 业务异常
+	 */
 	public static final String SERVICE_ERROR_MSG = "业务逻辑验证异常";
+	
+	/**
+	 * token 验证异常
+	 */
+	public static final String TOKEN_ERROR = "0003"; 
 }
