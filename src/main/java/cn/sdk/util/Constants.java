37a728cb0e69215d1c27e8bd6787692e7885d035
zan yang <yangzan_one@163.com>
2017-05-27 17:25:43 +0800

    文章状态常量修改

--- src/main/java/cn/sdk/util/Constants.java
+++ src/main/java/cn/sdk/util/Constants.java
@@ -35,37 +35,36 @@ public class Constants {
 	 * 默认页大小
 	 */
 	private static final int PAGESIZE_DEFAULT_5 = 5; 
-	
-	private static final int PAGESIZE_DEFAULT_10 = 10; 
+	public static final int PAGESIZE_DEFAULT_10 = 10; 
 	
 	/**
 	 * 草稿箱
 	 */
-	private static final int MAIN_STATUS_00 = 00;  
+	private static final String MAIN_STATUS_00 = "00";  
 	/**
 	 * 审核中
 	 */
-	private static final int MAIN_STATUS_01 = 01;  
+	private static final String MAIN_STATUS_01 = "01";  
 	/**
 	 * 申请撤回中
 	 */
-	private static final int MAIN_STATUS_02 = 02; 
+	private static final String MAIN_STATUS_02 = "02"; 
 	/**
 	 * 申请删除中
 	 */
-	private static final int MAIN_STATUS_03 = 03; 
+	private static final String MAIN_STATUS_03 = "03"; 
 	/**
 	 * 驳回
 	 */
-	private static final int MAIN_STATUS_04 = 04;
+	private static final String MAIN_STATUS_04 = "04";
 	/**
 	 * 审核通过 待下级审核
 	 */
-	private static final int MAIN_STATUS_05 = 05; 
+	private static final String MAIN_STATUS_05 = "05"; 
 	/**
 	 * 审核通过 已发布	
 	 */
-	private static final int MAIN_STATUS_06 = 06;
+	private static final String MAIN_STATUS_06 = "06";
 	
 	public static Map<Object, Object> SEX_MAP = new HashMap<Object, Object>();
 	
