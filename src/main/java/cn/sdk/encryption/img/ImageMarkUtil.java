f70e49b5dc168f579cf3cc43e1e11df599ee49db
qinyongbo <qinyongbo@chudaokeji.com>
2017-07-12 11:41:36 +0800

    添加随手拍图片水印

--- src/main/java/cn/sdk/encryption/img/ImageMarkUtil.java
+++ src/main/java/cn/sdk/encryption/img/ImageMarkUtil.java
@@ -15,6 +15,8 @@ import java.io.OutputStream;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 
+import cn.sdk.util.DateUtil2;
+
 //import com.founder.cms.watermark.util.ImageMarkLogoUtil;
 
 /**
@@ -23,7 +25,8 @@ import javax.swing.ImageIcon;
  * 
  */
 public class ImageMarkUtil {
-
+	private static final String PICTRUE_FORMATE_JPG = "jpg";
+	
 	/** 水印透明度 */
 	private static float alpha = 0.5f;
 	/** 水印图片旋转角度 */
@@ -162,6 +165,84 @@ public class ImageMarkUtil {
 			}
 		}
 	}
+	
+	/**
+	 * 获取字符长度，一个汉字作为 1 个字符, 一个英文字母作为 0.5 个字符
+	 * 
+	 * @param text
+	 * @return 字符长度，如：text="中国",返回 2；text="test",返回 2；text="中国ABC",返回 4.
+	 */
+	public static int getLength(String text) {
+		int textLength = text.length();
+		int length = textLength;
+		for (int i = 0; i < textLength; i++) {
+			if (String.valueOf(text.charAt(i)).getBytes().length > 1) {
+				length++;
+			}
+		}
+		return (length % 2 == 0) ? length / 2 : length / 2 + 1;
+	}
+	
+	/**
+	 * 添加文字水印
+	 * 
+	 * @param targetImg
+	 *            目标图片路径，如：C://myPictrue//1.jpg
+	 * @param pressText
+	 *            水印文字， 如：中国证券网
+	 * @param fontName
+	 *            字体名称， 如：宋体
+	 * @param fontStyle
+	 *            字体样式，如：粗体和斜体(Font.BOLD|Font.ITALIC)
+	 * @param fontSize
+	 *            字体大小，单位为像素
+	 * @param color
+	 *            字体颜色
+	 * @param x
+	 *            水印文字距离目标图片左侧的偏移量，如果x<0, 则在正中间
+	 * @param y
+	 *            水印文字距离目标图片上侧的偏移量，如果y<0, 则在正中间
+	 * @param alpha
+	 *            透明度(0.0 -- 1.0, 0.0为完全透明，1.0为完全不透明)
+	 */
+	public static void pressText(String targetImg, String pressText, String fontName, int fontStyle, int fontSize,
+			Color color, int x, int y, float alpha) {
+		try {
+			File file = new File(targetImg);
+
+			Image image = ImageIO.read(file);
+			int width = image.getWidth(null);
+			int height = image.getHeight(null);
+			BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+			Graphics2D g = bufferedImage.createGraphics();
+			g.drawImage(image, 0, 0, width, height, null);
+			g.setFont(new Font(fontName, fontStyle, fontSize));
+			g.setColor(color);
+			g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, alpha));
+
+			int width_1 = fontSize * getLength(pressText);
+			int height_1 = fontSize;
+			int widthDiff = width - width_1;
+			int heightDiff = height - height_1;
+			if (x < 0) {
+				x = widthDiff / 2;
+			} else if (x > widthDiff) {
+				x = widthDiff;
+			}
+			if (y < 0) {
+				y = heightDiff / 2;
+			} else if (y > heightDiff) {
+				y = heightDiff;
+			}
+
+			g.drawString(pressText, x, y + height_1);
+			g.dispose();
+			ImageIO.write(bufferedImage, PICTRUE_FORMATE_JPG, file);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 
 	public static void main(String[] args) {
 
@@ -172,8 +253,15 @@ public class ImageMarkUtil {
 		//修改默认参数
 		ImageMarkUtil.setImageMarkOptions(0.0f, 0,60);
 		String watermarkPath = "d:/11094340_0Vsb.jpg";  //测试水印图片
-        String imgPath = "d:/14958633330244TG0d.jpg";//测试需加水印图片
+        String imgPath = "d:/14958633330244TG0d.jpg";   //测试需加水印图片
 		ImageMarkUtil.waterMarkByImg(watermarkPath, imgPath);
+		
+		
+		//添加文字水印
+		pressText(watermarkPath, DateUtil2.getLastTimeInterval().get(1), "宋体", Font.BOLD, 30, Color.RED, -1, -1, 1.0f);
+		pressText(watermarkPath, "深圳南山区xxxxxx", "宋体", Font.BOLD, 30, Color.YELLOW, 10, 10, 1.0f);
+		
+		
 		System.out.println("..添加水印图片结束...");
 	}
 
