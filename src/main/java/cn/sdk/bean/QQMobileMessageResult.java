65e902147fe3c03ee9af6d288159c64a485ff551
gaoxigang <gaoxigang@chudaokeji.com>
2017-10-01 11:38:38 +0800

    增加腾讯云短信工具

---
+++ src/main/java/cn/sdk/bean/QQMobileMessageResult.java
@@ -0,0 +1,37 @@
+package cn.sdk.bean;
+
+public class QQMobileMessageResult {
+	private int result;
+	private String errmsg;
+	private String sid;
+	private int fee;
+	public int getResult() {
+		return result;
+	}
+	public void setResult(int result) {
+		this.result = result;
+	}
+	public String getErrmsg() {
+		return errmsg;
+	}
+	public void setErrmsg(String errmsg) {
+		this.errmsg = errmsg;
+	}
+	public String getSid() {
+		return sid;
+	}
+	public void setSid(String sid) {
+		this.sid = sid;
+	}
+	public int getFee() {
+		return fee;
+	}
+	public void setFee(int fee) {
+		this.fee = fee;
+	}
+	@Override
+	public String toString() {
+		return "QQMobileMessageResult [result=" + result + ", errmsg=" + errmsg
+				+ ", sid=" + sid + ", fee=" + fee + "]";
+	}
+}
