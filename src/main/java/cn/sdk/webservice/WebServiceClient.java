c5942d026d5083b3dbbe1bb938d2daa547d1389a
jiangjiayi <jiangjiayi@chudaokeji.com>
2017-11-07 10:43:13 +0800

    新增webservice超时接口

--- src/main/java/cn/sdk/webservice/WebServiceClient.java
+++ src/main/java/cn/sdk/webservice/WebServiceClient.java
@@ -474,9 +474,111 @@ public class WebServiceClient {
 		try {  
             Service service = new Service();
             Call call = (Call) service.createCall() ;
-            call.setTimeout(30000);
             call.setTargetEndpointAddress(url) ;  
             call.setOperationName(method) ;//ws方法名  
+            //一个输入参数,如果方法有多个参数,复制多条该代码即可,参数传入下面new Object后面
+            call.addParameter("userid",org.apache.axis.encoding.XMLType.XSD_DATE,javax.xml.rpc.ParameterMode.IN);
+            call.addParameter("userpwd",org.apache.axis.encoding.XMLType.XSD_DATE,javax.xml.rpc.ParameterMode.IN);
+            call.addParameter("jkid",org.apache.axis.encoding.XMLType.XSD_DATE,javax.xml.rpc.ParameterMode.IN);
+            call.addParameter("srcs",org.apache.axis.encoding.XMLType.XSD_DATE,javax.xml.rpc.ParameterMode.IN);
+            call.setReturnType(XMLType.XSD_STRING);
+            call.setUseSOAPAction(true);
+            
+            long startTime = System.currentTimeMillis();
+			respXml = (String) call.invoke(new Object[]{userid,userpwd,jkid,srcs});
+            long endTime = System.currentTimeMillis();
+            long result = (endTime - startTime) / 1000;
+            if(result >= 30 && !notHaveTime.contains(jkid)){
+            	logger.info(jkid + "接口编号执行耗时:" + result + " 秒");
+            	throw new WebServiceException(Integer.valueOf(MsgCode.webServiceCallError), MsgCode.webServiceCallMsg);
+            }
+            if(!jkids.contains(jkid)){
+            	logger.info("响应的xml：" + respXml);
+            }
+            //解密
+            Document doc= DocumentHelper.parseText(respXml);
+            Xml2Json.dom4j2Json(doc.getRootElement(),json);
+            
+            //返回的数据
+            String msg = (String) json.get("msg");
+            //返回的状态码
+            //解密
+        	respJson = DESCorder.decryptMode(msg,key, "utf-8");
+        	//System.out.println(respJson);
+        	
+            Document doc1 = DocumentHelper.parseText(respJson);
+        	Xml2Json.dom4j2Json(doc1.getRootElement(),json2);
+        	
+        	if(!jkids.contains(jkid)){
+            	logger.info("xml转换成json：" + json2);
+            }
+        } catch (Exception e) {
+        	logger.error("webservice调用错误,url=" + url + ",method=" + method + ",jkid=" + jkid + ",xml=" + "xml不打印" + ",userid=" + userid + ",userpwd=" + userpwd + ",key=" + key,e);
+            throw new WebServiceException(Integer.valueOf(MsgCode.webServiceCallError), MsgCode.webServiceCallMsg);
+        }  
+		return json2;
+	}
+    
+    /**
+     * webservice请求,携带超时时间(毫秒)
+     * @param url 请求url
+     * @param method 方法名称
+     * @param jkid 接口编号
+     * @param xml 组装的xml参数
+     * @param userid 用户名
+     * @param userpwd 密码
+     * @param key 秘钥
+     * @param timeout 超时时间(毫秒)
+     * @return json字符串
+     * @throws Exception
+     */
+    public static JSONObject requestWebServiceWithTimeout(String url,String method,String jkid,String xml,String userid,String userpwd,String key,int timeout) throws Exception{
+    	filterInterfaceLog();
+    	//url = "http://123.56.180.216:19002/xxfbpt/services/xxfbptservices";
+    	String base64Str = "";
+    	String logXml = xml;
+    	/*String reqXml = filterBase64(xml, replaceNodes);
+    	if(StringUtils.isNotBlank(reqXml)){
+    		logger.info("请求xml为：" + reqXml);
+    	}else{
+    		logger.info("请求xml为：" + xml);
+    	}*/
+    	//logger.info("调试日志：filterImgNodes" + filterImgNodes.toString());
+    	/*if(null != filterImgNodes){
+    		for(String filterImgNode : filterImgNodes){
+    			if(xml.contains(filterImgNode)){
+    				base64Str = StringUtils.substringBetween(xml, "<"+ filterImgNode + ">", "</"+ filterImgNode + ">");
+    				logXml = logXml.replace(base64Str, imgMsg);
+    			}
+    		}
+    	}*/
+    	/*if(null != filterImgNodes){
+    		for(String filterImgNode : filterImgNodes){
+    			if(!xml.contains(filterImgNode)){
+    				logger.info("requestWebService请求的xml：" + logXml);
+    			}
+    		}
+    	}*/
+    	
+		String respXml = "";
+		String respJson = "";
+		JSONObject json = new JSONObject();
+		JSONObject json2 = new JSONObject();
+		String srcs = DESCorder.encryptModeToString(xml,key);
+		try {  
+            Service service = new Service();
+            Call call = (Call) service.createCall();
+            //设置超时时间
+            if(timeout < 5000){
+            	timeout = 5000;
+            	call.setTimeout(timeout);
+            }else{
+            	call.setTimeout(timeout);
+            }
+            logger.info("接口编号(" + jkid + ")设置webservice超时时间：" + timeout + "ms");
+            
+            call.setTargetEndpointAddress(url);
+            call.setOperationName(method);//ws方法名  
             //一个输入参数,如果方法有多个参数,复制多条该代码即可,参数传入下面new Object后面
             call.addParameter("userid",org.apache.axis.encoding.XMLType.XSD_DATE,javax.xml.rpc.ParameterMode.IN);
             call.addParameter("userpwd",org.apache.axis.encoding.XMLType.XSD_DATE,javax.xml.rpc.ParameterMode.IN);
@@ -519,6 +621,7 @@ public class WebServiceClient {
         }  
 		return json2;
 	}
+    
     
     /**
      * webservice请求
