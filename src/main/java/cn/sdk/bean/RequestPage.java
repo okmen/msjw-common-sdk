8ca9859d7a3e46de3def53f65e3fefacc9f10182
wumeng <369840065@qq.com>
2018-01-23 21:24:44 +0800

    Update RequestPage.java

--- src/main/java/cn/sdk/bean/RequestPage.java
+++ src/main/java/cn/sdk/bean/RequestPage.java
@@ -2,7 +2,7 @@ package cn.sdk.bean;
 
 import java.io.Serializable;
 public class RequestPage implements Serializable{
-	private static final long serialVersionUID = -216551289066136262L;
+	private static final long serialVersionUID = 216551289066136262L;
 	//当前页
 	protected int page;
 	//页大小
